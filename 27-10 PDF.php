3b1d6bbe615ece8d12479ad004f030dfe904953d
Starchaser1993 <97558693+Starchaser1993@users.noreply.github.com>
2022-10-27 00:42:37 -0300

    PDF

---
+++ 27-10 PDF.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    
+ </body>
+ </html>
+<?php
+
+//Trabajo con archivos PDF
+/*Crear un PDF que contenga gráficos y texto
+Darle un estilo al PDF que contenga gráficos y texto
+Darle un estilo al PDF con CSS y una estructura con HTML/Librería para que contenga un titulo
+un resumen, luego incluir un gráfico y luego un pie de página con el nombre del curso y
+ la fecha en la cual se generó el PDF(la fecha debe generarse automáticamente, con PHP)*/
+ 
